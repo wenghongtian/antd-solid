be93059d0b0f98fcf59dd5c0b4a8e3b4f6b688dd
wenghongtian <wenghongtian@github.com>
2023-06-28 18:53:28 +0800

    feat: 代码

--- packages/sc-util/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as warning } from './warning';
-export * from './warning';
-export { default as isEqual } from './isEqual';
-export { default as canUseDom } from './Dom/canUseDom';
-export { default as contains } from './Dom/contains';
-export * from './Dom/dynamicCSS';
-export { default as raf } from './raf';
-export * from './Dom/styleChecker';
